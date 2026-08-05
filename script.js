d222908e4970de37605d4094a15c80cb6f769f63
shop365india <shreebalajient.india@gmail.com>
2026-08-05 12:32:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+console.log("SHOP365 INDIA Loaded Successfully");
+
+const button = document.querySelector(".hero button");
+
+if(button){
+
+button.addEventListener("click",()=>{
+
+alert("Welcome to SHOP365 INDIA!\nProducts will be available soon.");
+
+});
+
+}
+
+});
