171630d2a9165161952ea3191af3ab4cbe638109
shop365india <shreebalajient.india@gmail.com>
2026-08-05 16:31:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,29 @@ if (sellerForm) {
     });
 }
 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzT0ANkDzSjYz4ZraKnnofO3yCMMoZyvaL-IS2Dnrwv4jUUCXkhII1JUyQrMVppwIJt/exec";
+
+// Seller Form
+const sellerForm = document.getElementById("sellerForm");
+
+if (sellerForm) {
+    sellerForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        fetch(SCRIPT_URL, {
+            method: "POST",
+            body: new FormData(sellerForm)
+        })
+        .then(() => {
+            alert("Seller Registration Submitted Successfully!");
+            sellerForm.reset();
+        })
+        .catch(() => {
+            alert("Something went wrong.");
+        });
+    });
+}
+
 // Requirement Form
 const requirementForm = document.getElementById("requirementForm");
 
@@ -35,6 +58,27 @@ if (requirementForm) {
         .then(() => {
             alert("Requirement Submitted Successfully!");
             requirementForm.reset();
+        })
+        .catch(() => {
+            alert("Something went wrong.");
+        });
+    });
+}
+
+// Product Form
+const productForm = document.getElementById("productForm");
+
+if (productForm) {
+    productForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        fetch(SCRIPT_URL, {
+            method: "POST",
+            body: new FormData(productForm)
+        })
+        .then(() => {
+            alert("Product Added Successfully!");
+            productForm.reset();
         })
         .catch(() => {
             alert("Something went wrong.");
